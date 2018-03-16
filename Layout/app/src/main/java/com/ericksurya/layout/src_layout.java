cb5ebe41b73c66da03346b5760801bc70f75c383
proesd <ericksuryadinata@gmail.com>
2018-03-16 07:43:57 +0700

    initial commit

---
+++ Layout/app/src/main/java/com/ericksurya/layout/src_layout.java
@@ -0,0 +1,13 @@
+package com.ericksurya.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class src_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frm_layout);
+    }
+}
